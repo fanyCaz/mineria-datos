c847d5f1818e02c2e661b85c09476945ccd34002
fanyCaz <stepheniecaz@gmail.com>
2022-04-27 21:50:43 -0500

    finding new centroids

--- k_means/main.py
+++ k_means/main.py
@@ -1,25 +1,50 @@
-from preprocessing import calculate_distances, bias, slope, adjustment
+from preprocessing import calculate_distances, normalize, slope, new_centroids
 from kmeans import run 
 import numpy as np
 import pandas as pd
+from refining import refine
 
 matrix = pd.read_csv('hoax.csv')
-
 matrix = np.array(matrix)
 
-center = 1.56
-calculate_distances(matrix,center,88)
-bias(matrix[:,0],center)
-bias(matrix[:,1],88)
-slope(matrix[:,0])
-m,b = slope(matrix[:,1])
-adjusted = adjustment(m,b,matrix[:,1])
+norm_matrix = normalize(matrix)
 
-number_klusters = 4
+centers = np.array([[0,0],[1,1]])
 
-data = np.concatenate([[0.3*np.random.randn(2) for i in range(200)],[[1,1] + 0.3*np.random.randn(2) for i in range(200)], [[1,-1]+0.3*np.random.randn(2) for i in range(200)]])
+distances = calculate_distances(norm_matrix,centers)
+new_centroids(distances,centers)
+#for center in centers:
+#  distances.append( calculate_distances(norm_matrix,center) )
 
-centroids = data[:number_klusters]
-result = run(data, centroids)
 
-print( result )
+#result = calculate_distances(matrix,center,88)
+#print(result)
+#bias(matrix[:,0],centers[0])
+#bias(matrix[:,1],88)
+
+#m,b = slope(matrix[:,0])
+#norm_data1 = normalize(matrix[:,0],m,b)
+#m,b = slope(matrix[:,1])
+#norm_data2 = normalize(matrix[:,1],m,b)
+
+#m = np.array([norm_data1,norm_data2])
+
+#d = calculate_distances(m,centers[0])
+#print( d )
+#d = calculate_distances(m,centers[1])
+#print( d )
+
+#number_klusters = 4
+
+# our take
+#data = np.concatenate([[0.3*np.random.randn(2) for i in range(200)],[[1,1] + 0.3*np.random.randn(2) for i in range(200)], [[1,-1]+0.3*np.random.randn(2) for i in range(200)]])
+
+#centroids = data[:number_klusters]
+#result = run(data, centroids)
+
+#print( result )
+
+# refinenment
+
+#refine(1,data,number_klusters,2)
+
--- k_means/preprocessing.py
+++ k_means/preprocessing.py
@@ -1,23 +1,42 @@
 import numpy as np
 import math
 
-def calculate_distances(matrix, center: float, bias: float) -> list:
-  distances = list(map(lambda index: math.sqrt( math.pow(index[0]-center,2) + math.pow(index[1]-bias,2) ) ,matrix))
+def calculate_distances(matrix, centers) -> list:
+  distances = []
+  for idx,row in enumerate(matrix):
+    temp = []
+    for center in centers:
+      value = math.sqrt( math.pow(center[0] - row[0],2) + math.pow(center[1] - row[1],2) )
+      temp.append(value)
+    distances.append( temp )
+  distances = np.array(distances)
   return distances
 
-def bias(column: list, center: float) -> list:
-  biases = list(map(lambda value: math.pow(value-center,2),column ))
-  return biases
+def new_centroids(distances,centers):
+  min_indexes_distances = np.argmin(distances,axis=1)
+  print( np.amin(distances,axis=1) )
+  min_distances = np.amin(distances,axis=1)
+  temp = []
+  for idx,row in enumerate(distances):
+    for c_temp in enumerate(centers):
+      value = [ min_distances[idx] == c_temp]
+      print( value) 
 
-def slope(column: list,y0: int = 0, y1: int = 1):
+  sum_indexes = sum(min_indexes_distances)
+  return
+
+def slope(column: list,y0: float = 0, y1: float = 1):
   x0 = min(column)
   x1 = max(column)
   m = (y1-y0)/(x1 - x0)
-  b = -m*x0
+  b = y0-m*x0
   return m,b
 
-def adjustment(m: int,b: int, originals: list):
-  adjusted = list(map(lambda original: m*original+b,originals))
-  return adjusted
-
+def normalize(matrix: list):
+  slopes = [ slope(matrix[:,idx]) for idx in range(len(matrix[0])) ]
+  for idx,row in enumerate(matrix):
+    for jdx,element in enumerate(row): 
+      value = slopes[jdx][0]*element + slopes[jdx][1]
+      matrix[idx][jdx] = value
+  return matrix
 
