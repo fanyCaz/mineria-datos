577b910503e4b4ae0b1f9c222eed05e1c3cb6d82
fanyCaz <stepheniecaz@gmail.com>
2022-05-04 20:12:41 -0500

    scoped queries on numbers

--- association_rule/combinaciones.py
+++ association_rule/combinaciones.py
@@ -27,18 +27,19 @@ for idx,variable in enumerate(variable_combinations):
   rule_if = variable[0]
   rule_then = variable[1]
   print(f'if {rule_if} then {rule_then}')
-
-  for element_if in grouping[rule_if]["elements"]:
+  min_q = 0
+  for idx,element_if in enumerate(grouping[rule_if]["elements"]):
     print(f'{rule_if} -> {element_if}')
-    if grouping[rule_if]["g_type"] == "number":
-      if_data = data.query(f'{rule_if} <= {element_if}')
-    else:
-      if_data = data.query(f'{rule_if} == "{element_if}"')
-    for element_then in grouping[rule_then]["elements"]:
+    if idx > 0:
+      min_q = grouping[rule_if]["elements"][idx-1]
+    query = f'{rule_if} > {min_q} & {rule_if} <= {element_if}' if grouping[rule_if]["g_type"] == "number" else f'{rule_if} == "{element_if}"' 
+    if_data = data.query(query)
+    min_q_j = 0
+    for jdx,element_then in enumerate(grouping[rule_then]["elements"]):
       print(f'{rule_then} -> {element_then}')
-      if grouping[rule_then]["g_type"] == "number":
-        then_data = if_data.query(f'{rule_then} <= {element_then}')
-      else:
-        then_data = if_data.query(f'{rule_then} == "{element_then}"')
+      if jdx > 0:
+        min_q_j = grouping[rule_then]["elements"][jdx -1]
+      query = f'{rule_then} > {min_q_j} & {rule_then} <= {element_then}' if grouping[rule_then]["g_type"] == "number" else f'{rule_then} == "{element_then}"' 
+      then_data = if_data.query(query)
       print( len(then_data) )
 
