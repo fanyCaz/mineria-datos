dac497caf0562f59d733ab8624e0515b9ce1ff41
fanyCaz <stepheniecaz@gmail.com>
2022-05-04 19:59:23 -0500

    all combinations

--- association_rule/combinaciones.py
+++ association_rule/combinaciones.py
@@ -27,26 +27,18 @@ for idx,variable in enumerate(variable_combinations):
   rule_if = variable[0]
   rule_then = variable[1]
   print(f'if {rule_if} then {rule_then}')
-  print(f'{rule_if} = {grouping[rule_if]["elements"][0]}')
 
-  for i in grouping[rule_if]["elements"]:
-    print(i)
-    for j in grouping[rule_then]["elements"]:
-      print(j)
-
-  if grouping[rule_if]["g_type"] == "number":
-    if_data = data.query(f'{rule_if} <= {grouping[rule_if]["elements"][0]}')
-    # llamar a función que haga ciclo entre todos los elementos
-  else:
-    if_data = data.query(f'{rule_if} == "{grouping[rule_if]["elements"][0]}"')
-  if grouping[rule_then]["g_type"] == "number":
-    then_data = if_data.query(f'{rule_then} <= {grouping[rule_then]["elements"][0]}')
-  else:
-    then_data = if_data.query(f'{rule_then} == "{grouping[rule_then]["elements"][0]}"')
-  print( len(then_data) )
-
-
-# llamar a función que haga ciclo entre todos los elementos
-# después solo contar los que cumplan con ambos, con uno de cada uno y el total de elementos
-# pregunta: los cuantiles valen como suficiente para dividir los datos? Si es suficiente para las variables que son como continuas, se dejará así para el sake del programa
+  for element_if in grouping[rule_if]["elements"]:
+    print(f'{rule_if} -> {element_if}')
+    if grouping[rule_if]["g_type"] == "number":
+      if_data = data.query(f'{rule_if} <= {element_if}')
+    else:
+      if_data = data.query(f'{rule_if} == "{element_if}"')
+    for element_then in grouping[rule_then]["elements"]:
+      print(f'{rule_then} -> {element_then}')
+      if grouping[rule_then]["g_type"] == "number":
+        then_data = if_data.query(f'{rule_then} <= {element_then}')
+      else:
+        then_data = if_data.query(f'{rule_then} == "{element_then}"')
+      print( len(then_data) )
 
