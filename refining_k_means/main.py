34e682e73566db975854979aa046e0f80a5fe113
fanyCaz <stepheniecaz@gmail.com>
2022-05-15 23:41:31 -0500

    kmeansmod beggining

--- refining_k_means/main.py
+++ refining_k_means/main.py
@@ -1,4 +1,5 @@
 from preprocessing import calculate_distances, normalize, new_centroids
+from refining import refine
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -39,6 +40,28 @@ def kmeans(norm_matrix,centers):
   print("Elementos de cada centroide")
   print(elements)
 
+def kmeansMod(start_point,sample,k):
+  j_objective = 0
+  j_ant = sys.maxsize
+  belonging = []
+  count = 0
+  while True:
+    count+=1
+    distances = calculate_distances(norm_matrix,centers)
+    centers, j_objective, belonging = new_centroids(distances,centers,norm_matrix,j_ant)
+    if math.isclose(j_objective,j_ant,rel_tol=0.0001):
+      break
+    else:
+      j_ant = j_objective
+  print(f"Para {len(centers)} centros, el mejor objetivo logrado fue : {j_objective}; el número de iteraciones fueron: {count}")
+  print("Los centros son:")
+  print(centers)
+
+  elements = centroids_belonging(belonging)
+  print("Elementos de cada centroide")
+  print(elements)  
+  return elements
+
 def read_data(file_name):
   matrix = []
   debug = True
@@ -54,14 +77,14 @@ def read_data(file_name):
   return matrix
 
 matrix = read_data('segmentation_paper.csv')
-matrix = np.array(matrix)
-matrix = matrix.astype('float64')
-#print(matrix)
+matrix = np.array(matrix,dtype = 'float64')
 norm_matrix = normalize(matrix)
 #centers = np.array([[0,0,0],[1,1,1]])
 #centers = np.array([[0,0,0],[1,1,1],[0.5,0.5,0.5]])
 #centers = np.array([[0,0,0],[1,1,1],[0,1,1],[1,0,1]])
 #centers = np.array([[0,0,0],[1,1,1],[0,1,1],[1,0,0],[0.5,0.5,0.5]])
 centers = np.array([[0,0,0],[1,1,1],[0,1,1],[1,0,1],[0.5,0.5,0.5],[0.2,0.2,0.2]])
+k = len(centers)
+#kmeans(norm_matrix,centers)
+refine(centers,norm_matrix,k)
 
-kmeans(norm_matrix,centers)
--- refining_k_means/refining.py
+++ refining_k_means/refining.py
@@ -1,20 +1,19 @@
 import numpy as np
 from numpy import ndarray as nd
-from kmeans import run
-
-# say J subsamples
+from main import kmeans, kmeansMod
 
 def refine(initial_start_point, data, k, num_subsamples=2):
-  print(data)
   cm = {}
   flatten_data = nd.flatten(data)
-  limit = np.random.randint(1,len(data)/2)
+  #limit = np.random.randint(1,len(data)/2)
+  max_rows = 5
   for i in range(num_subsamples):
-    s_i = np.random.choice(flatten_data, limit)
-    print(s_i)
-    print("result")
-    print(s_i[:k])
-    result = run(np.array(s_i),s_i[:k])
-    print(result)
+    #s_i = np.random.choice(flatten_data, limit)
+    rand_start = np.random.randint(1,max_rows)
+    s_i = data[rand_start:rand_start+max_rows]
+    print(f'subsample {s_i} stat {rand_start}')
+    cm_i = kmeansMod(initial_start_point,s_i,k)
+    cm = cm + cm_i
 
   return
+
