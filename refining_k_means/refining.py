45dabc51b5db704affd6c2b91a41c1a27aa6efbe
fanyCaz <stepheniecaz@gmail.com>
2022-05-17 20:51:07 -0500

    return elements to cm

--- refining_k_means/refining.py
+++ refining_k_means/refining.py
@@ -4,21 +4,21 @@ from numpy import ndarray as nd
 import sys
 import math
 
-def refine(initial_start_point, data, k, num_subsamples=2):
-  cm = {1}
+def refine(initial_start_point, data, k, num_subsamples=4):
+  cm = []
   flatten_data = nd.flatten(data)
   print(f'centros iniciales {initial_start_point}')
   #limit = np.random.randint(1,len(data)/2)
-  max_rows = 5
+  max_rows = 10
   #for i in range(num_subsamples):
-  for i in range(2):
+  centers = initial_start_point
+  for i in range(num_subsamples):
     #s_i = np.random.choice(flatten_data, limit)
     rand_start = np.random.randint(1,max_rows)
     s_i = data[rand_start:rand_start+max_rows]
     print(f'subsample {s_i} stat {rand_start}')
-    cm_i = kmeansMod(initial_start_point,s_i,k)
-    cm = cm.union(cm_i)
-
+    elements, centers, j_obj = kmeansMod(centers,s_i,k)
+    cm.append(centers)
   return
 
 def kmeans(norm_matrix,centers):
@@ -41,6 +41,7 @@ def kmeans(norm_matrix,centers):
   elements = centroids_belonging(belonging)
   print("Elementos de cada centroide")
   print(elements)
+  return elements, centers, j_objective
 
 def kmeansMod(start_point,sample,k):
   j_objective = 0
@@ -53,18 +54,10 @@ def kmeansMod(start_point,sample,k):
     distances = calculate_distances(sample,centers)
     centers, j_objective, belonging = new_centroids(distances,centers,sample,j_ant)
     if math.isclose(j_objective,j_ant,rel_tol=0.0001):
-      print("Pertenencia")
-      print(belonging)
-      print("Los centros ANTES DE  son:")
-      print(centers)
       empty_cluster = any([ sum(belonging[:,i]) == 0 for i in range(len(belonging[0])) ])
       if empty_cluster:
-        print(f'distancias \n{distances}')
-        # deben ser los maximos
         mini = np.max(distances)
         farthest_distances = np.argmax(distances,axis=1)
-        print(f'minims {mini} y {farthest_distances}')
-        print(f'del sample {sample[farthest_distances[0]]}')
         for idx, center in enumerate(centers):
           if sum(belonging[:,idx]) == 0:
             centers[idx] = sample[farthest_distances[idx]]
@@ -78,7 +71,7 @@ def kmeansMod(start_point,sample,k):
   elements = centroids_belonging(belonging)
   print("Elementos de cada centroide")
   print(elements)  
-  return elements, centers
+  return elements, centers, j_objective
 
 def centroids_belonging(belonging: list):
   identifiers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V']
