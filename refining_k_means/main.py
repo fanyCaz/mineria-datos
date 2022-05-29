0367707100c0e08f20cec1b14e9fc2bee8759a67
fanyCaz <stepheniecaz@gmail.com>
2022-05-29 17:42:09 -0500

    move input function to utils

--- refining_k_means/main.py
+++ refining_k_means/main.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
-from utils import generador
+from utils import generador, input_normalized
 
 # una vez obtenido el scalar de distortion entonces ya se puede aplicar kmeans a toda la base de datos, 
 # esta parte primero hay que guardar los centros en un txt y de ahi los leo para kmeans general
@@ -21,21 +21,6 @@ def read_data(file_name):
   except:
     raise FileNotFoundError
   return matrix
-
-def input_normalized(question: str, extra_constraint=None):
-  centers_iniciales = input(question)
-  try:
-    if int(centers_iniciales) < 0:
-      return input_normalized(question)
-    else:
-      if extra_constraint:
-        if int(centers_iniciales) < extra_constraint[0] or int(centers_iniciales) > 17:
-          print(f"Ingresa un número entre {extra_constraint[0]} y {17}")
-          return input_normalized(question, extra_constraint)
-      return int(centers_iniciales)
-  except:
-    print('Ingresa un para los centros , porfavor')
-    return input_normalized(question)
   
 matrix = read_data('segmentation_paper.csv')
 matrix = np.array(matrix,dtype = 'float64')
--- refining_k_means/utils.py
+++ refining_k_means/utils.py
@@ -12,3 +12,21 @@ def generador(numero_centros, numero_coordenadas):
   centros=np.random.rand(numero_centros, numero_coordenadas)
   return centros
 
+"""
+Optional: extra_constraint can receive an array of two elements,
+the minimum value and the maximum value like: [0,2]
+"""
+def input_normalized(question: str, extra_constraint=None):
+  centers_iniciales = input(question)
+  try:
+    if int(centers_iniciales) < 0:
+      return input_normalized(question)
+    else:
+      if extra_constraint:
+        if int(centers_iniciales) < extra_constraint[0] or int(centers_iniciales) > 17:
+          print(f"Ingresa un número entre {extra_constraint[0]} y {17}")
+          return input_normalized(question, extra_constraint)
+      return int(centers_iniciales)
+  except:
+    print('Ingresa un para los centros , porfavor')
+    return input_normalized(question)
