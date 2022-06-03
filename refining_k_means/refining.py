532dfca45bafc02b2540d7b1b17e9cd4ea7df4ad
fanyCaz <stepheniecaz@gmail.com>
2022-06-03 14:50:28 -0500

    simplified elements

--- refining_k_means/refining.py
+++ refining_k_means/refining.py
@@ -11,7 +11,7 @@ def refine(initial_start_point, data, k, num_subsamples=4, max_rows=10):
   centers = initial_start_point
   for i in range(num_subsamples):
     s_i = get_subsample(data,max_rows)
-    elements, centers, j_obj, belonging = kmeansMod(centers,s_i,k)
+    elements, centers, j_obj, belonging, s_elements = kmeansMod(centers,s_i,k)
     save = {'elements': s_i, 'centers': centers}
     cm.append(save)
   #idx_min_distortion,min_distortion = distortion(cm)
@@ -22,7 +22,7 @@ def refine(initial_start_point, data, k, num_subsamples=4, max_rows=10):
   #print(cm)
   for j in range(num_subsamples):
     centers = cm[j]['centers']
-    elements, centers, j_obj, belonging = kmeans(cm[j]['elements'],centers)
+    elements, centers, j_obj, belonging, s_elements = kmeans(cm[j]['elements'],centers)
     save = {'elements': cm[j]['elements'], 'centers': centers}
     fms.append(save)
   #print("resultados despues")
@@ -52,8 +52,8 @@ def kmeans(norm_matrix,centers):
       break
     else:
       j_ant = j_objective
-  elements = centroids_belonging(belonging, distances)
-  return elements, centers, j_objective, belonging
+  elements, s_elements = centroids_belonging(belonging, distances)
+  return elements, centers, j_objective, belonging, s_elements
 
 def kmeansMod(start_point,sample,k):
   j_objective = 0
@@ -66,15 +66,15 @@ def kmeansMod(start_point,sample,k):
     distances = calculate_distances(sample,centers)
     centers, j_objective, belonging = new_centroids(distances,centers,sample,j_ant)
     if math.isclose(j_objective,j_ant,rel_tol=0.0001):
-      elements = centroids_belonging(belonging, distances)
+      elements, s_elements = centroids_belonging(belonging, distances)
       empty_cluster = len(elements) != len(centers) # asking if not all clusters has elements
       if empty_cluster:
         centers = update_empty_centers(centers,belonging,elements,sample)
       break
     else:
       j_ant = j_objective
-  elements = centroids_belonging(belonging, distances)
-  return elements, centers, j_objective, belonging
+  elements, s_elements = centroids_belonging(belonging, distances)
+  return elements, centers, j_objective, belonging, s_elements
 
 def update_empty_centers(centers, belonging,elements,sample):
   farthest_distance = []
@@ -97,16 +97,18 @@ def update_empty_centers(centers, belonging,elements,sample):
 def centroids_belonging(belonging: list, distances: list):
   identifiers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V']
   elements = {}
+  simplified_elements = {}
   for idx,element in enumerate(belonging):
     centroid_number = np.argmax(element)
     try:
       elements[centroid_number].append({'idx':idx, 'distance': np.min(distances[idx]) })
-      #elements[centroid_number].append({'idx':idx })
+      simplified_elements[centroid_number].append(idx)
     except:
       elements[centroid_number] = []
       elements[centroid_number].append({'idx':idx, 'distance': np.min(distances[idx]) })
-      #elements[centroid_number].append({'idx':idx })
-  return elements
+      simplified_elements[centroid_number] = []
+      simplified_elements[centroid_number].append(idx)
+  return elements, simplified_elements
 
 def distortion(k_solution, smooth_solution):
   distortions_fm = []
